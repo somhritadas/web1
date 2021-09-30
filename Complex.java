69ee9c2bd225e6d8fdc7c3fb1f1ce61dbcc7543c
imharshraj02 <91587576+imharshraj02@users.noreply.github.com>
2021-10-01 00:27:15 +0530

    Update complex.java by imharshraj02

--- Complex.java
+++ Complex.java
@@ -14,8 +14,14 @@ System.out.println("Sum is: "+real+" + "+img+"i");
 public static void main(String args[])
 {
 
-double n1,n2,n3,n4; n1=DOUBLE.parseDouble(args[0]); n2=Double.parseDouble(args[1]);
-n3=Double.parseDouble(args[2]); n4=Double.parseDouble(args[3]); Complex num1 = new Complex(n1,n2); Complex num2 = new Complex(n3,n4); num1.add(num2);
+double n1,n2,n3,n4;
+ n1=DOUBLE.parseDouble(args[0]);
+ n2=Double.parseDouble(args[1]);
+n3=Double.parseDouble(args[2]); 
+ n4=Double.parseDouble(args[3]); 
+ Complex num1 = new Complex(n1,n2);
+ Complex num2 = new Complex(n3,n4); 
+ num1.add(num2);
 num1.display();
 
 
